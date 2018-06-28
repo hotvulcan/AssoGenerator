306ef53496eb924f4903a1026dc468589d80de17
Victor@Mac27 <hotvulcan@gmail.com>
2018-06-28 12:34:51 +0800

    try to support camel style params

--- lib/generators/asso/asso_generator.rb
+++ lib/generators/asso/asso_generator.rb
@@ -141,27 +141,24 @@ require 'rails/generators/base'
       the_left_class = left_class
       relation,right_class,option,*option_vars = relation.split ":"
       right_class = right_class.singularize.camelcase
-      #print "\n>",relation,",",right_class,",",option,",",option_vars,"\n"
       #2.1 check if the models is there. die if none.
       #todo just die now
       right_obj   = right_class.constantize
 
-      #2.1 check if the relations is made already.
-      # todo, later
+      #2.1 TODO check if the relations is made already.
       #2.2 detemine what relation it will be. add reverse relations to jobs
       print "found ", relation," to ",right_class,"\n"
       do_add_relation(the_left_class,relation,right_class,option,option_vars)
-      if ["has_one_me","has_me_only","has_only_me","has_one"].include? option 
+      if ["HasOneMe","HasMeOnly","HasOlnyMe","HasOne","hasone","has_one_me","has_me_only","has_only_me","has_one"].include? option 
         relation="has_one"
         right_class,the_left_class=the_left_class,right_class #swap left and right
         do_add_relation(the_left_class,relation,right_class,option,option_vars)
       end
-      if ["belongs_to_me"].include? option
+      if ["BelongsToMe","belongstome","BelongsMe","belongs_me","belongs_to_me"].include? option
         relation="belongs_to"
         right_class,the_left_class=the_left_class,right_class #swap left and right
         do_add_relation(the_left_class,relation,right_class,option,option_vars)
       end
-
     end 
 
     
